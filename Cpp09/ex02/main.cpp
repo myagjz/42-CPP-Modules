2ed9637c907d52d0b86de136ffa89501cb17aa4e
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2024-05-17 22:59:21 +0300

    Add files via upload

---
+++ Cpp09/ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myagiz <myagiz@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 20:32:50 by myagiz            #+#    #+#             */
+/*   Updated: 2024/05/17 22:51:32 by myagiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int main(int argc, char **argv) {
+	PmergeMe merge;
+
+	if (argc < 3)
+	{
+		std::cout << "Error" << std::endl;
+		return (0);
+	}
+	merge.start(argc,argv);
+	merge.process();
+	return (0);
+}
